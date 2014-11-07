897db691906c55a3a4f00a002be398a2a87aa556
ashwanikhare <ashwanikhare.nitw@gmail.com>
2014-11-07 00:32:39 +0000

    Add order_of_freq_item_id (descending order of frequent itemid)

--- fp_growth.cpp
+++ fp_growth.cpp
@@ -4,7 +4,7 @@
  */
 #include "fp_tree_ds.hpp"
 
-using namespace std;
+ using namespace std;
 #define MAPI map<int,int>
 #define FOR_MAP(ii,structure,T) for(structure::iterator (ii)=(T).begin();(ii)!=(T).end();(ii)++)
 #define FOR_next_MAP(jj,ii,structure,T) for(structure::iterator (jj)=(ii);(jj)!=(T).end();(jj)++)
@@ -12,7 +12,11 @@ using namespace std;
 /**
  * minimum support count
  */
-const int MIN_SUP = 2;
+ const int MIN_SUP = 2;
+/**
+ * contains freq item in descending order
+ */
+ vector<pair<int,bool> > order_of_freq_item_id;
 
 
 /**
@@ -20,35 +24,48 @@ const int MIN_SUP = 2;
  * Collect F, frequent_items and their support count.
  * sort F in support count descending order as L.
  */
+void scan_dataset();
+/**
+ * generate order list of frequent data items in desc order
+ * @param map_freq_set contains frequent data-item and their count
+ */
+void form_desc_order_list_of_freq_item(MAPI map_freq_set);
 
-void scan_dataset()
-{
+ int main(int argc, char const *argv[])
+ {
+
+ 	scan_dataset();
+ 	return 0;
+ }
+
+ void scan_dataset()
+ {
 	/**
 	 * map_freq_set contains freq_item_id and their support count
 	 * support count >= MIN_SUP
 	 */
-	MAPI map_freq_set;
-	map_freq_set.clear();
+	 MAPI map_freq_set;
+	 map_freq_set.clear();
 
 
-	ifstream fin;
+	 ifstream fin;
 	/**
 	 * dataset file name
 	 */
-	fin.open("input.txt");
+	 fin.open("input.txt");
 
-	if(!fin)
-	{
-		cout<<"ErrorID:FPG_1 Opening input file\n";
-		exit(0);
-	}
+	 if(!fin)
+	 {
+	 	cout<<"ErrorID:FPG_1 Opening input file\n";
+	 	exit(0);
+	 }
 
-	int item_id;
+	 int item_id;
 
-	while(fin>>item_id)
-	{
-		if(item_id!=-1)
-		{
+	 while(fin>>item_id)
+	 {
+	 	if(item_id!=-1)
+	 	{
 			if(map_freq_set.count(item_id)>0)//if item_id is already present
 				map_freq_set[item_id]++;
 			else
@@ -70,13 +87,37 @@ void scan_dataset()
 
 	map_freq_set.clear();
 	map_freq_set=map_temp;
+	form_desc_order_list_of_freq_item(map_freq_set);
 
-	
 }
 
-int main(int argc, char const *argv[])
+void form_desc_order_list_of_freq_item(MAPI map_freq_set)
 {
+	order_of_freq_item_id.clear();
 	
-	scan_dataset();
-	return 0;
+	FOR_MAP(ii,MAPI,map_freq_set)
+	{
+		int max_count = 0;
+		int max_item_id = 0;
+
+		FOR_MAP(jj,MAPI,map_freq_set)
+		{
+			if(jj->second > max_count)
+			{
+				max_count = jj->second;
+				max_item_id = jj->first;
+			}
+		}
+
+		if (max_item_id > 0)
+		{
+			order_of_freq_item_id.push_back(make_pair(max_item_id,false));
+			map_freq_set[max_item_id]=-1;
+		}
+	}
+
+	/*for (int i = 0; i < order_of_freq_item_id.size(); ++i)
+	{
+		cout<<order_of_freq_item_id[i].first<<" "<<order_of_freq_item_id[i].second<<"\n";
+	}*/
 }
